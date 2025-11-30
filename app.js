e97f258de658ed5b6228736f419fe15068ce9d53
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2025-11-30 22:15:09 +0530

    Third Commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let weather= document.getElementById("txtName").value;
+
+function getWeather(){
+    fetch(`http://api.weatherapi.com/v1/current.json?key=d9a23dbaf6e94aa791290458251211&q=`+txtName.vale)
+    .
+}
